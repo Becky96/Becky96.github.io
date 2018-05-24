05b01a96ff972b7cfaf3a7777078188cc9a18d2d
Becky Johnson <becky-j_@hotmail.com>
2018-05-24 08:20:21 +0100

    Updating

--- sketch.js
+++ sketch.js
@@ -1,31 +1,8 @@
-
-<script>
+/*<script>
 $(document).ready(function(){
     $("button").click(function(){
         $("p").hide();
     });
 });
 
-
-$(document).ready(function()
-{
-    $("a").hover(function(e)
-    {
-        var randomClass = getRandomClass();
-        $(e.target).attr("class", randomClass);
-    });
-});
-
-
-function getRandomClass()
-{
-    //Store available css classes
-    var classes = new Array("green", "purple", "teal", "violet", "pink");
-    
-    //Give a random number from 0 to 5
-    var randomNumber = Math.floor(Math.random()*6);
-    
-    return classes[randomNumber];
-
-}
-</script>
+*/
